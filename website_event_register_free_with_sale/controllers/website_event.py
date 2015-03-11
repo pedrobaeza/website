221da94bf316e62d7ab6b2fc1b40f3030f410128
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-03-11 21:38:13 +0100

    [IMP] website_event_register_free_with_sale: Use same checkout screen for both paid and free tickets

--- website_event_register_free_with_sale/controllers/website_event.py
+++ website_event_register_free_with_sale/controllers/website_event.py
@@ -19,7 +19,7 @@
 #
 ##############################################################################
 from openerp import http
-from openerp.addons.web.http import request
+from openerp.http import request
 from openerp.addons.website_event_sale.controllers.main import website_event
 
 
@@ -27,9 +27,7 @@ class WebsiteEvent(website_event):
     @http.route(['/event/cart/update'], type='http', auth="public",
                 methods=['POST'], website=True)
     def cart_update(self, **post):
-        event_id = long(post['event_id'])
-        # First pass - Check if there is any free ticket
-        has_free_tickets = False
+        has_paid_tickets = False
         for key, value in post.items():
             qty = int(value or "0")
             ticket_words = key.split("-")
@@ -39,27 +37,18 @@ class WebsiteEvent(website_event):
                 continue
             ticket = request.env['event.event.ticket'].sudo().browse(ticket_id)
             if not ticket.price:
-                has_free_tickets = True
-        # Second pass - Add registrations / order lines
-        for key, value in post.items():
-            qty = int(value or "0")
-            ticket_words = key.split("-")
-            ticket_id = (ticket_words[0] == 'ticket' and
-                         int(ticket_words[1]) or None)
-            if not qty or not ticket_id:
-                continue
-            ticket = request.env['event.event.ticket'].sudo().browse(ticket_id)
-            if not ticket.price:
-                # It's a free event
-                event = request.env['event.event'].sudo().browse(event_id)
                 # Accumulate possible multiple free tickets
-                post['tickets'] = str(int(post.get('tickets', '0')) + qty)
-            elif has_free_tickets:
+                post['free_tickets'] = (
+                    str(int(post.get('free_tickets', '0')) + qty))
+            else:
+                has_paid_tickets = True
                 # Add to shopping cart the rest of the items
                 order = request.website.sale_get_order(force_create=1)
                 order.with_context(event_ticket_id=ticket.id)._cart_update(
                     product_id=ticket.product_id.id, add_qty=qty)
-        if post.get('tickets'):
-            return self.event_register_free(event, **post)
-        else:
-            return super(WebsiteEvent, self).cart_update(**post)
+        request.session.update({
+            'free_tickets': post['free_tickets'],
+            'event_id': post['event_id'],
+            'has_paid_tickets': has_paid_tickets,
+        })
+        return request.redirect("/shop/checkout")
